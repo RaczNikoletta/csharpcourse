be076e715e23f1a94c48ff77019cf611e69bddcb
Rácz Nikoletta <44648333+motypython@users.noreply.github.com>
2022-09-15 11:46:04 +0200

    Add project files.

---
+++ countObjects/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace countObjects
+{
+    class Program
+    {
+        static int Counter = 0;
+
+        public Program()
+        {
+            Counter++;
+            Console.WriteLine("Number of objects:: {0}", Counter);
+
+        }
+
+        ~Program()
+        {
+            Counter--;
+            Console.WriteLine("Number of objects:: {0}", Counter);
+        }
+        static void Main(string[] args)
+        {
+           while(true)
+            {
+                Program temp = new Program();
+            }
+        }
+    }
+}
